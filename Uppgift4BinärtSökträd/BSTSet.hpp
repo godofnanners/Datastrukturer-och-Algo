d467df868a84aec0ff5abef1358e7923da5a0a6e
godofnanners <casper.martensson@telia.com>
2020-03-16 08:19:15 +0100

    Update BSTSet.hpp

--- Uppgift4BinärtSökträd/BSTSet.hpp
+++ Uppgift4BinärtSökträd/BSTSet.hpp
@@ -3,11 +3,10 @@
 template <class T>
 class BSTSetNode {
 	template <class U>
-	friend class DoublyLinkedList;
+	friend class BSTSet;
 
 	BSTSetNode(T aValue);
 	~BSTSetNode() {};
-
 	T myValue;
 	BSTSetNode<T>* myRightNode;
 	BSTSetNode<T>* myLeftNode;
@@ -35,7 +34,7 @@ public:
 	//Plockar bort elementet ur mängden, om det finns. Gör ingenting annars.
 	void Remove(const T& aValue);
 private:
-	BSTSetNode<T>*myRootNode;	
+	BSTSetNode<T>* myRootNode;
 };
 
 template<class T>
@@ -53,9 +52,20 @@ template<class T>
 inline bool BSTSet<T>::HasElement(const T& aValue)
 {
 	BSTSetNode<T>* Node = myRootNode;
-	while (myRootNode)
+	while (Node != nullptr)
 	{
-
+		if (!(Node->myValue < aValue) && !(aValue < Node->myValue))
+		{
+			return true;
+		}
+		else if (Node->myValue < aValue)
+		{
+			Node = Node->myLeftNode;
+		}
+		else
+		{
+			Node = Node->myRightNode;
+		}
 	}
 	return false;
 }
@@ -63,11 +73,35 @@ inline bool BSTSet<T>::HasElement(const T& aValue)
 template<class T>
 inline void BSTSet<T>::Insert(const T& aValue)
 {
-
+	BSTSetNode<T>* Node = myRootNode;
+	while (!(Node->myValue < aValue) && !(aValue < Node->myValue))
+	{
+		if (Node->myValue < aValue)
+		{
+			if (Node->myLeftNode==nullptr)
+			{
+				Node->myLeftNode = new BSTSetNode<T>(aValue);
+			}
+			Node = Node->myLeftNode;
+		}
+		else if(aValue < Node->myValue)
+		{
+			if (Node->myLeftNode == nullptr)
+			{
+				Node->myLeftNode = new BSTSetNode<T>(aValue);
+			}
+			Node = Node->myRightNode;
+		}	
+	}
+	
 }
 
 template<class T>
 inline void BSTSet<T>::Remove(const T& aValue)
 {
+	while (!(Node->myValue < aValue) && !(aValue < Node->myValue))
+	{
+
+	}
 }
 

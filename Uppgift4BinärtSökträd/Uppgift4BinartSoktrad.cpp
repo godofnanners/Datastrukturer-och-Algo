3b208ef691ced4d2fcf81f3899c46f9ba43e4f51
godofnanners <casper.martensson@telia.com>
2020-03-24 02:47:16 +0100

    backtracking sudukosolver done

--- Uppgift4BinärtSökträd/Uppgift4BinartSoktrad.cpp
+++ Uppgift4BinärtSökträd/Uppgift4BinartSoktrad.cpp
@@ -8,48 +8,58 @@ namespace Uppgift4BinartSoktrad
 	TEST_CLASS(Uppgift4BinartSoktrad)
 	{
 	public:
-		
+
 		TEST_METHOD(Test_BSTSetTree_Insert)
 		{
-			CommonUtilities::BSTSet<float> myTree;
-			myTree.Insert(5);
-			myTree.Insert(3);
-			myTree.Insert(4);
-			myTree.Insert(1);
-			myTree.Insert(10);
-			myTree.Insert(11);
-			myTree.Insert(7);
-			myTree.Insert(9);
-			myTree.Insert(6);
-			Assert::IsTrue(myTree.HasElement(5));
-			Assert::IsTrue(myTree.HasElement(3));
-			Assert::IsTrue(myTree.HasElement(4));
-			Assert::IsTrue(myTree.HasElement(1));
-			Assert::IsTrue(myTree.HasElement(10));
-			Assert::IsTrue(myTree.HasElement(11));
-			Assert::IsTrue(myTree.HasElement(7));
-			Assert::IsTrue(myTree.HasElement(9));
-			Assert::IsTrue(myTree.HasElement(6));
-			Assert::IsFalse(myTree.HasElement(2));
-			Assert::IsFalse(myTree.HasElement(12));
-			Assert::IsFalse(myTree.HasElement(8));
+			_CrtMemState memStart;
+			_CrtMemState memEnd;
+			_CrtMemState memDiff;
+			_CrtMemCheckpoint(&memStart);
+			{
+				CommonUtilities::BSTSet<float> myTree;
+				myTree.Insert(5);
+				myTree.Insert(3);
+				myTree.Insert(4);
+				myTree.Insert(1);
+				myTree.Insert(10);
+				myTree.Insert(11);
+				myTree.Insert(7);
+				myTree.Insert(9);
+				myTree.Insert(6);
+				
+
+				myTree.Remove(5);
+				myTree.Remove(3);
+				myTree.Remove(4);
+				myTree.Remove(1);
+				myTree.Remove(10);
+				myTree.Remove(11);
+				myTree.Remove(7);
+				myTree.Remove(9);
+				myTree.Remove(6);
+			}
+
+			_CrtMemCheckpoint(&memEnd);
+			Assert::IsFalse(_CrtMemDifference(&memDiff, &memStart, &memEnd), L"Tree creates memory leak!");
 		}
 		TEST_METHOD(Test_BSTSetTree_Remove)
 		{
 			CommonUtilities::BSTSet<float> myTree;
 			myTree.Insert(5);
-			myTree.Insert(3);
+			/*myTree.Insert(3);
 			myTree.Insert(4);
 			myTree.Insert(1);
 			myTree.Insert(10);
 			myTree.Insert(11);
 			myTree.Insert(7);
 			myTree.Insert(9);
-			myTree.Insert(6);
+			myTree.Insert(6);*/
 
 			myTree.Remove(5);
-			
-		
+
+			myTree.Insert(7);
+			Assert::IsTrue(myTree.HasElement(7));
+
 		}
 	};
 }

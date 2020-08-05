546d9b3c4fed19838d92783ef64b531a77436518
godofnanners <casper.martensson@telia.com>
2020-08-05 22:51:58 +0200

    uppgift 7 klar

---
+++ Uppgift8–Sortering/Uppgift8–Sortering.cpp
@@ -0,0 +1,16 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Uppgift8Sortering
+{
+	TEST_CLASS(Uppgift8Sortering)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
